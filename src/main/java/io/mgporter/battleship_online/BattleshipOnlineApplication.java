d2e0c0ed9d58f5b9c98735c972999dee2f5fed77
mgporter <26415062+mgporter@users.noreply.github.com>
2023-10-24 14:38:26 -0400

    re-initialize repo

---
+++ src/main/java/io/mgporter/battleship_online/BattleshipOnlineApplication.java
@@ -0,0 +1,13 @@
+package io.mgporter.battleship_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mgporter/battleship_online/BattleshipOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mgporter.battleship_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
